cdc38210fc7d616c7f9855431b650eec51ef8fe5
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2025-06-20 23:56:30 +0530

    Fixed Railway Issue

--- server/src/index.js
+++ server/src/index.js
@@ -23,6 +23,6 @@ app.use('/api/sessions',  sessionRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Backend listening on http://localhost:${PORT}`);
 });
